ca953a600c66b66f9655120a3239d177ab9bb281
iloveangpao <yanrui.lim@gmail.com>
2025-09-23 11:50:48 +0800

    awg_spi

---
+++ projects/awg_spi/kcu116/system_bd.tcl
@@ -0,0 +1,43 @@
+# modified by Prerna Baranwal for kcu116 - ad9144
+
+# TODO: modified this for dac fifo 
+set dac_fifo_address_width 14
+## Offload attributes
+#set adc_offload_type 
+#set adc_offload_size [expr 512 * 1024]
+
+
+#TODO: left below, cross verification, 
+#TODO: setting up the fifo algo 
+#set dac_offload_type 0
+#set dac_offload_size [expr 1 * 1024 * 1024]
+
+#set plddr_offload_axi_data_width 0
+
+
+
+source $ad_hdl_dir/projects/common/kcu116/kcu116_system_bd.tcl
+source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+source ../common/awg_bd.tcl
+source $ad_hdl_dir/projects/scripts/adi_pd.tcl
+
+#system ID
+ad_ip_parameter axi_sysid_0 CONFIG.ROM_ADDR_BITS 9
+ad_ip_parameter rom_sys_0 CONFIG.PATH_TO_FILE "[pwd]/mem_init_sys.txt"
+ad_ip_parameter rom_sys_0 CONFIG.ROM_ADDR_BITS 9
+
+#set sys_cstring "ADC_OFFLOAD_TYPE=$adc_offload_type\nDAC_OFFLOAD_TYPE=$dac_offload_type"
+
+# TODO: modified it for making it DAC agnostic
+set ADI_DAC_DEVICE $::env(ADI_DAC_DEVICE)
+set ADI_DAC_MODE $::env(ADI_DAC_MODE)
+set sys_cstring "$ADI_DAC_DEVICE - $ADI_DAC_MODE"
+sysid_gen_sys_init_file $sys_cstring
+ad_ip_parameter axi_ad9144_jesd/tx CONFIG.SYSREF_IOB false
+
+
+ad_ip_parameter util_awg_xcvr CONFIG.QPLL_FBDIV 20
+ad_ip_parameter util_awg_xcvr CONFIG.QPLL_REFCLK_DIV 1
+ad_ip_parameter util_awg_xcvr CONFIG.CPLL_CFG0 0x67f8
+ad_ip_parameter util_awg_xcvr CONFIG.CPLL_CFG1 0xa4ac
+ad_ip_parameter util_awg_xcvr CONFIG.CPLL_CFG2 0x0007
---
+++ projects/awg_spi/kcu116/system_project.tcl
@@ -0,0 +1,37 @@
+# modified by Prerna Baranwal for kcu 116, ad 9144 
+source ../../../scripts/adi_env.tcl
+source $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
+source $ad_hdl_dir/projects/scripts/adi_board.tcl
+source ../common/config.tcl
+
+# get_env_param retrieves parameter value from the environment if exists,
+# other case use the default value
+#
+
+
+# Parameter description:
+#  JESD_M : Number of converters per link
+#   JESD_L : Number of lanes per link
+#   JESD_S : Number of samples per frame
+
+adi_project awg_kcu116 0 [list \
+  JESD_M    [get_config_param M] \
+  JESD_L    [get_config_param L] \
+  JESD_S    [get_config_param S] \
+  JESD_NP   [get_config_param NP] \
+  NUM_LINKS $num_links \
+  DEVICE_CODE $device_code \
+]
+
+adi_project_files awg_kcu116 [list \
+"../common/awg_spi.v" \
+  "system_top.v" \
+  "system_constr.xdc"\
+  "$ad_hdl_dir/library/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/kcu116/kcu116_system_constr.xdc" ]
+
+## To improve timing in DDR4 MIG
+set_property strategy Performance_Retiming [get_runs impl_1]
+
+adi_project_run awg_kcu116
+
